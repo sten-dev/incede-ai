ce994d9d433d1a5ff322cd11915a34a3823fcf75
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2019-12-19 10:40:06 +0530

    added bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,53 +34,56 @@ const Navbar = class extends React.Component {
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
+      <React.Fragment>
+
+        <nav
+          className="navbar is-transparent"
+          role="navigation"
+          aria-label="main-navigation"
+        >
+          <div className="container">
+            <div className="navbar-brand">
+              <Link to="/" className="navbar-item" title="Logo">
+                <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              </Link>
+              {/* Hamburger menu */}
+              <div
+                className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+                data-target="navMenu"
+                onClick={() => this.toggleHamburger()}
+              >
+                <span />
+                <span />
+                <span />
+              </div>
+            </div>
             <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
+              id="navMenu"
+              className={`navbar-menu ${this.state.navBarActiveClass}`}
             >
-              <span />
-              <span />
-              <span />
+              <div className="navbar-start has-text-centered">
+              </div>
+              <div className="navbar-end has-text-centered">
+                <Link className="navbar-item" to="/about">
+                  About
+              </Link>
+                <Link className="navbar-item" to="/products">
+                  Products
+              </Link>
+                <Link className="navbar-item" to="/blog">
+                  Blog
+              </Link>
+                <Link className="navbar-item" to="/contact">
+                  Contact
+              </Link>
+                <Link className="navbar-item" to="/contact/examples">
+                  Form Examples
+              </Link>
+              </div>
             </div>
           </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-            </div>
-            <div className="navbar-end has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </React.Fragment>
     )
   }
 }
