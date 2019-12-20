34697315ae550294f256c5c786270f435430f353
Suraj <esuraj@stuworx.com>
2019-12-20 14:27:56 +0530

    css fixes

--- src/components/Landing/BotSection.js
+++ src/components/Landing/BotSection.js
@@ -13,23 +13,29 @@ class BotSection extends Component {
       <section className="bot">
         <Container>
           <Row>
-            <Col className="d-flex justify-content-center">
-              <img
-                src={logo}
-                alt="incede.ai"
-                style={{ width: "10em", height: "6.5em" }}
-              />
+            <Col className="d-flex flex-column">
+              <div className="d-flex justify-content-center flex-grow-1">
+                <img
+                  src={logo}
+                  alt="incede.ai"
+                  style={{ width: "10em", height: "5em" }}
+                />
+              </div>
+              <div className="d-flex justify-content-center flex-grow-1">
+                <p className="lead text-white">
+                  Incede provides outcome-based AI solutions and offers
+                </p>
+              </div>
+              <br />
             </Col>
           </Row>
           <Row>
             <Col>
-              <section className="d-flex flex-column flex-grow-1">
+              <section className="chat d-flex flex-column flex-grow-1">
                 <ChatPill text="Our solutions provide outsome-based answers to the business problems." />
                 <ChatPill text="Which solutions are you looking for?" />
-                <ChatPill
-                  right
-                  text="Customer Analytics"
-                />
+                <ChatPill right text="Customer Analytics" />
+                <ChatPill text="You can use our customer analytics solution to target the right customers with predictive modeling. Identify dissatisfied customers by uncovering patterns of behavior. Address customer service issues faster by correlating and analyzing a variety of data." />
               </section>
             </Col>
           </Row>
