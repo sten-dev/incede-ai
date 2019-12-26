17a80a47d652af6af6bfc7a9ef7cc5e0791a7e55
Suraj <esuraj@stuworx.com>
2019-12-26 15:04:08 +0530

    added youtube link

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,25 +1,26 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+import CMS from "netlify-cms-app";
+import uploadcare from "netlify-cms-media-library-uploadcare";
+import cloudinary from "netlify-cms-media-library-cloudinary";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
-import SolutionsPostPreview from './preview-templates/SolutionsPostPreview'
-import CaseStudyPreview from './preview-templates/CaseStudyPreview'
-import ServicePreview from './preview-templates/ServicePreview'
-import IndustriesPreview from './preview-templates/IndustryPreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import ProductPagePreview from "./preview-templates/ProductPagePreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import SolutionsPostPreview from "./preview-templates/SolutionsPostPreview";
+import CaseStudyPreview from "./preview-templates/CaseStudyPreview";
+import ServicePreview from "./preview-templates/ServicePreview";
+import IndustriesPreview from "./preview-templates/IndustryPreview";
+import { YoutubeLink } from "./editor-components/youtubeLink";
 
+CMS.registerEditorComponent(YoutubeLink);
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
-
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('solutions', SolutionsPostPreview)
-CMS.registerPreviewTemplate('services', ServicePreview)
-CMS.registerPreviewTemplate('industries', IndustriesPreview)
-CMS.registerPreviewTemplate('case-study', CaseStudyPreview)
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("products", ProductPagePreview);
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
+CMS.registerPreviewTemplate("solutions", SolutionsPostPreview);
+CMS.registerPreviewTemplate("services", ServicePreview);
+CMS.registerPreviewTemplate("industries", IndustriesPreview);
+CMS.registerPreviewTemplate("case-study", CaseStudyPreview);
