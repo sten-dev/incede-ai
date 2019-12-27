a2f144b21d4ef2d7e843ac4fdf7e0aff31739f43
Suraj <esuraj@stuworx.com>
2019-12-27 11:54:49 +0530

    added view page  for solution view

--- src/templates/solutions-post.js
+++ src/templates/solutions-post.js
@@ -1,73 +1,79 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+import { Col, Row, Container } from "reactstrap";
 
 export const SolutionsPostTemplate = ({
-    subTitle,
-    title,
-    content,
-    contentComponent,
-    helmet,
+  subTitle,
+  title,
+  content,
+  contentComponent,
+  helmet
 }) => {
-    const PageContent = contentComponent || Content;
-    return (
-        <section className="section">
-            {helmet || ''}
-            <div className="container content">
-                <div className="columns">
-                    <div className="column is-10 is-offset-1">
-                        <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-                            {title}
-                        </h1>
-                        <p>{subTitle}</p>
-                    </div>
-                    <PageContent className="content" content={content} />
-                </div>
-            </div>
-        </section>
-    )
-}
+  const PageContent = contentComponent || Content;
+  return (
+    <section className="solution-page">
+      {helmet || ""}
+      <section className="header-section gap-y">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="display-4">{title}</h1>
+              <h2>{subTitle}</h2>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <Container>
+        <Row>
+          <Col>
+            <main className="content gap-y-half">
+              <PageContent content={content} />
+            </main>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
 SolutionsPostTemplate.propTypes = {
-    title: PropTypes.string,
-    subTitle: PropTypes.string,
-    helmet: PropTypes.object,
-}
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  helmet: PropTypes.object
+};
 
 const SolutionsPost = ({ data }) => {
-    const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
-    return (
-        <Layout>
-            <SolutionsPostTemplate
-                content={post.html}
-                contentComponent={HTMLContent}
-                subTitle={post.frontmatter.subTitle}
-                helmet={
-                    <Helmet titleTemplate="%s | Solution">
-                        <title>{`${post.frontmatter.title}`}</title>
-                        <meta
-                            name="description"
-                            content={`${post.frontmatter.subTitle}`}
-                        />
-                    </Helmet>
-                }
-                title={post.frontmatter.title}
-            />
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <SolutionsPostTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        subTitle={post.frontmatter.subTitle}
+        helmet={
+          <Helmet titleTemplate="%s | Solution">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta name="description" content={`${post.frontmatter.subTitle}`} />
+          </Helmet>
+        }
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  );
+};
 
 SolutionsPost.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.object,
-    }),
-}
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  })
+};
 
-export default SolutionsPost
+export default SolutionsPost;
 
 export const pageQuery = graphql`
   query SolutionsPostByID($id: String!) {
@@ -78,13 +84,13 @@ export const pageQuery = graphql`
         title
         subTitle
         image {
-            childImageSharp {
-              fluid(maxWidth: 240, quality: 64) {
-                ...GatsbyImageSharpFluid
-              }
+          childImageSharp {
+            fluid(maxWidth: 240, quality: 64) {
+              ...GatsbyImageSharpFluid
             }
+          }
         }
       }
     }
   }
-`
+`;
