bea949642b2c3c44a40ac4d36415a8723f05c122
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2020-01-02 10:31:54 +0530

    added solutions list

--- src/components/Solutions/SolutionsMain.js
+++ src/components/Solutions/SolutionsMain.js
@@ -1,38 +1,95 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql, StaticQuery } from 'gatsby'
-
+import { graphql, StaticQuery, Link } from 'gatsby'
+import { Container, Row, Col, Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+import PreviewCompatibleImage from '../PreviewCompatibleImage';
+import arrow from "../../img/arrow.svg"
+import "../../styles/solutions.scss";
 class SolutionsMain extends React.Component {
-    render() {
-        const { data } = this.props
-        const { edges: solutions } = data.allMarkdownRemark
-        console.log("solutions list", solutions)
-        return (
-            <div>
-
-            </div>
-        )
-    }
+  render() {
+    const { data } = this.props
+    const { edges: solutions } = data.allMarkdownRemark
+    console.log("solutions list", solutions)
+    return (
+      <section className="solutions-list ">
+        <section className="header-section gap-y text-center">
+          <Container>
+            <Row>
+              <Col>
+                <h1>Solutions</h1>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        <Container>
+          {solutions.map((x, i) => {
+            let solution = x.node.frontmatter;
+            return (
+              <Row className="solution-card" key={i}>
+                <Col>
+                  <Card>
+                    <CardBody className="p-0">
+                      <Row>
+                        <Col lg={2} md={2} sm={4} xs={12} className="image-section">
+                          <PreviewCompatibleImage
+                            imageInfo={{
+                              image: solution.image,
+                              alt: `thumbnail`,
+                              style: { width: "100%" }
+                            }}
+                          />
+                        </Col>
+                        <Col lg={10} md={10} sm={8} xs={12} className="card-container">
+                          <h5 className="mt-0">{solution.title}</h5>
+                          <h6 className="mt-0">{solution.subTitle}</h6>
+                          <CardText>
+                            {x.node.excerpt}
+                          </CardText>
+                          <br />
+                          <div>
+                            <Link to={x.node.fields.slug} className="btn btn-secondary btn-sm">
+                              Read More
+                            {"  "}
+                              <img src={arrow} alt="next" style={{ width: "12px", height: "12px", marginTop: -2 }} />
+                            </Link>
+                            {/* <button type="button" className="btn btn-secondary btn-sm" >
+                              Read More
+                            {"  "}
+                              <img src={arrow} alt="next" style={{ width: "12px", height: "12px", marginTop: -2 }} />
+                            </button> */}
+                          </div>
+                        </Col>
+                      </Row>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            );
+          })}
+        </Container>
+      </section>
+    )
+  }
 }
 
 SolutionsMain.propTypes = {
-    data: PropTypes.shape({
-        allMarkdownRemark: PropTypes.shape({
-            edges: PropTypes.array,
-        }),
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
     }),
+  }),
 }
 
 export default () => (
-    <StaticQuery
-        query={graphql`
+  <StaticQuery
+    query={graphql`
       query SolutionsMainQuery {
         allMarkdownRemark(
           filter: { frontmatter: { templateKey: { eq: "solutions-post" } } }
         ) {
           edges {
             node {
-              excerpt(pruneLength: 400)
+              excerpt(pruneLength: 200)
               id
               fields {
                 slug
@@ -54,6 +111,6 @@ export default () => (
         }
       }
     `}
-        render={(data) => <SolutionsMain data={data} />}
-    />
+    render={(data) => <SolutionsMain data={data} />}
+  />
 )
