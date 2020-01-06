1a04b0fa24dfe9613e8f9eb95e5d21dc514666f8
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2020-01-06 12:14:19 +0530

    added fav icon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,24 +17,21 @@ const Layout = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix("/")}img/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix("/")}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix("/")}img/favicon-16x16.png`}
-          sizes="16x16"
-        />
+        <link rel="apple-touch-icon" sizes="57x57" href={`${withPrefix("/")}img/fav/apple-icon-57x57.png`} />
+        <link rel="apple-touch-icon" sizes="60x60" href={`${withPrefix("/")}img/fav/apple-icon-60x60.png`} />
+        <link rel="apple-touch-icon" sizes="72x72" href={`${withPrefix("/")}img/fav/apple-icon-72x72.png`} />
+        <link rel="apple-touch-icon" sizes="76x76" href={`${withPrefix("/")}img/fav/apple-icon-76x76.png`} />
+        <link rel="apple-touch-icon" sizes="114x114" href={`${withPrefix("/")}img/fav/apple-icon-114x114.png`} />
+        <link rel="apple-touch-icon" sizes="120x120" href={`${withPrefix("/")}img/fav/apple-icon-120x120.png`} />
+        <link rel="apple-touch-icon" sizes="144x144" href={`${withPrefix("/")}img/fav/apple-icon-144x144.png`} />
+        <link rel="apple-touch-icon" sizes="152x152" href={`${withPrefix("/")}img/fav/apple-icon-152x152.png`} />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${withPrefix("/")}img/fav/apple-icon-180x180.png`} />
+        <link rel="icon" type="image/png" sizes="192x192" href={`${withPrefix("/")}img/fav/android-icon-192x192.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${withPrefix("/")}img/fav/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="96x96" href={`${withPrefix("/")}img/fav/favicon-96x96.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${withPrefix("/")}img/fav/favicon-16x16.png`} />
+        <link rel="manifest" href={`${withPrefix("/")}img/fav/manifest.json`} />
+        <meta name="msapplication-TileImage" content={`${withPrefix("/")}img/fav/ms-icon-144x144.png`}></meta>
 
         <link
           rel="mask-icon"
