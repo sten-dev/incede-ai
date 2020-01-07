b939e82a932d16c8c49646276a8f5381996ef3b1
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2020-01-07 18:48:03 +0530

    added locus solutions

--- src/components/ChatLocation.js
+++ src/components/ChatLocation.js
@@ -4,15 +4,15 @@ import location from "../img/location.png"
 const ChatLocation = () => {
     return (
         <div className="chat-location">
-            <a rel="noopener noreferrer" href={`https://www.google.com/maps/search/?api=1&query=17.369265,78.539638`} target="_blank">
+            <a rel="noopener noreferrer" href={`https://www.google.com/maps/search/?api=1&query=37.406306,-121.976493`} target="_blank">
                 <Card>
                     <CardImg top width="100%" src={location} alt="Card image cap" />
                     <CardBody>
                         <h6 className="m-0">
-                            <strong>ContentErra</strong>
+                            <strong>Locus Solutions</strong>
                         </h6>
                         <p>
-                            Prabhat Nagar, Laxmi Nagar Colony, Kothapet, Hyderabad, Telangana 500035
+                            5201 Great America Pkwy #320 Santa Clara, CA 95054
                     </p>
                     </CardBody>
                 </Card>
