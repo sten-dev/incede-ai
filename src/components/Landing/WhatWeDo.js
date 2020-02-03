4998555dafbc813f4c899e51cd240d9dd8706fba
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2020-02-03 19:24:10 +0530

    removed industries

--- src/components/Landing/WhatWeDo.js
+++ src/components/Landing/WhatWeDo.js
@@ -8,7 +8,7 @@ class WhatWeDo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedTab: "solutions"
+            selectedTab: "services"
         }
     }
     handleTabChange = (selectedTab) => {
@@ -27,9 +27,9 @@ class WhatWeDo extends Component {
                             </div>
                             <Col className="menu-tabs d-flex align-items-center">
                                 <div className="header w-100 d-flex">
-                                    <span onClick={() => this.handleTabChange("solutions")} className={`tab ${this.state.selectedTab === 'solutions' ? 'selected-tab' : ''}`}>Solutions</span>
                                     <span onClick={() => this.handleTabChange("services")} className={`tab ${this.state.selectedTab === 'services' ? 'selected-tab' : ''}`}>Services</span>
-                                    <span onClick={() => this.handleTabChange("industries")} className={`tab ${this.state.selectedTab === 'industries' ? 'selected-tab' : ''}`}>Industries</span>
+                                    <span onClick={() => this.handleTabChange("solutions")} className={`tab ${this.state.selectedTab === 'solutions' ? 'selected-tab' : ''}`}>Solutions</span>
+                                    {/* <span onClick={() => this.handleTabChange("industries")} className={`tab ${this.state.selectedTab === 'industries' ? 'selected-tab' : ''}`}>Industries</span> */}
                                 </div>
                             </Col>
                         </Row>
