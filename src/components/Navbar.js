fa0829edbecda3f7256d3fcbe404da416406485b
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2020-02-03 14:07:24 +0530

    removed nav bar if not logged in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,6 +24,7 @@ import ServicesMenu from "./menu/ServicesMenu";
 import IndustriesMenu from "./menu/IndustriesMenu";
 import dropdown from "../img/dropdown.svg";
 import MenuSection from "./menu/MenuSection";
+import { isTokenExpired } from "../Utils";
 const INavbar = props => {
   const [isOpen, setIsOpen] = useState(false);
   const [popoverOpen, setPopoverOpen] = useState(false);
@@ -32,100 +33,115 @@ const INavbar = props => {
 
   const toggleWhatWeDoMenu = () => setPopoverOpen(!popoverOpen);
   return (
-    <nav className="incede-nav">
-      <Navbar color="white" expand="lg" light>
-        <NavbarBrand>
-          <Link to="/">
-            <img src={logo} alt="incede" />
-          </Link>
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle}>
-          <img src={menu} alt="menu" />
-        </NavbarToggler>
-        <Collapse isOpen={false} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem className="btn">
-              <Link activeClassName="selected" to="/">Home</Link>
-            </NavItem>
-            <NavItem className="btn">
-              <Link activeClassName="selected" to="/about">About Us</Link>
-            </NavItem>
-            <NavItem id="whatWeDo" className="btn">
-              <a>
-                What We Do{" "}
-                <img
-                  className={`${popoverOpen ? "active-what-we-do" : ""}`}
-                  alt="send"
-                  src={dropdown}
-                // onClick={props.onClick}
-                />{" "}
-              </a>
-            </NavItem>
-            <NavItem className="btn">
-              <Link activeClassName="selected" to="/case-study">Case Studies</Link>
-            </NavItem>
-            {/* <NavItem className="btn">
+    <React.Fragment>
+      {isTokenExpired() ? (
+        <nav className="incede-nav">
+          <Navbar color="white" expand="lg" light>
+            <NavbarBrand>
+              <Link to="/">
+                <img src={logo} alt="incede" />
+              </Link>
+            </NavbarBrand>
+          </Navbar>
+        </nav>
+      ) : (
+          <nav className="incede-nav">
+            <Navbar color="white" expand="lg" light>
+              <NavbarBrand>
+                <Link to="/">
+                  <img src={logo} alt="incede" />
+                </Link>
+              </NavbarBrand>
+              <NavbarToggler onClick={toggle}>
+                <img src={menu} alt="menu" />
+              </NavbarToggler>
+              <Collapse isOpen={false} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem className="btn">
+                    <Link activeClassName="selected" to="/">Home</Link>
+                  </NavItem>
+                  <NavItem className="btn">
+                    <Link activeClassName="selected" to="/about">About Us</Link>
+                  </NavItem>
+                  <NavItem id="whatWeDo" className="btn">
+                    <a>
+                      What We Do{" "}
+                      <img
+                        className={`${popoverOpen ? "active-what-we-do" : ""}`}
+                        alt="send"
+                        src={dropdown}
+                      // onClick={props.onClick}
+                      />{" "}
+                    </a>
+                  </NavItem>
+                  <NavItem className="btn">
+                    <Link activeClassName="selected" to="/case-study">Case Studies</Link>
+                  </NavItem>
+                  {/* <NavItem className="btn">
               <Link activeClassName="selected" to="/customers">Our Customers</Link>
             </NavItem> */}
-            <NavItem className="btn">
-              <Link activeClassName="selected" to="/contact">Contact Us</Link>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+                  <NavItem className="btn">
+                    <Link activeClassName="selected" to="/contact">Contact Us</Link>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar>
 
-      <Popover
-        popperClassName="what-we-do-menu"
-        boundariesElement="window"
-        placement="bottom"
-        isOpen={popoverOpen}
-        target="whatWeDo"
-        toggle={toggleWhatWeDoMenu}
-        trigger="legacy"
-      >
-        {/* <PopoverHeader>Popover Title</PopoverHeader> */}
-        <PopoverBody>
-          <Container fluid className="p-4">
-            <Row>
-              <Col lg={3} md={3} sm={12} xs={12}>
-                <h1 className="text-gray d-none d-sm-block">
-                  What we do
+            <Popover
+              popperClassName="what-we-do-menu"
+              boundariesElement="window"
+              placement="bottom"
+              isOpen={popoverOpen}
+              target="whatWeDo"
+              toggle={toggleWhatWeDoMenu}
+              trigger="legacy"
+            >
+              {/* <PopoverHeader>Popover Title</PopoverHeader> */}
+              <PopoverBody>
+                <Container fluid className="p-4">
+                  <Row>
+                    <Col lg={3} md={3} sm={12} xs={12}>
+                      <h1 className="text-gray d-none d-sm-block">
+                        What we do
                 </h1>
-                <h4 onClick={toggleWhatWeDoMenu} className="d-block d-sm-none">
-                  <img className="back-icon" alt="send" src={dropdown} /> What
-                  We Do
+                      <h4 onClick={toggleWhatWeDoMenu} className="d-block d-sm-none">
+                        <img className="back-icon" alt="send" src={dropdown} /> What
+                        We Do
                 </h4>
-              </Col>
-              <br className="d-block d-md-none" />
-              <br className="d-block d-md-none" />
-              <br className="d-block d-md-none" />
-              <Col className="wwd-sub-menu" lg={3} md={3} sm={6} xs={12}>
-                <h5 className="mt-0">Services</h5>
-                <ServicesMenu />
-              </Col>
-              <Col className="wwd-sub-menu" lg={3} md={3} sm={6} xs={12}>
-                <h5 className="mt-0">Solutions</h5>
-                <SolutionsMenu />
-              </Col>
-              <Col className="wwd-sub-menu" lg={3} md={3} sm={6} xs={12}>
-                <h5 className="mt-0">Industries</h5>
-                <IndustriesMenu />
-              </Col>
-            </Row>
-          </Container>
-        </PopoverBody>
-      </Popover>
-      <Modal
-        isOpen={isOpen}
-        toggle={toggle}
-        className="bot-side-menu"
-        backdrop="static"
-      >
-        <ModalBody>
-          <MenuSection toggle={toggle} />
-        </ModalBody>
-      </Modal>
-    </nav>
+                    </Col>
+                    <br className="d-block d-md-none" />
+                    <br className="d-block d-md-none" />
+                    <br className="d-block d-md-none" />
+                    <Col className="wwd-sub-menu" lg={3} md={3} sm={6} xs={12}>
+                      <h5 className="mt-0">Services</h5>
+                      <ServicesMenu />
+                    </Col>
+                    <Col className="wwd-sub-menu" lg={3} md={3} sm={6} xs={12}>
+                      <h5 className="mt-0">Solutions</h5>
+                      <SolutionsMenu />
+                    </Col>
+                    <Col className="wwd-sub-menu" lg={3} md={3} sm={6} xs={12}>
+                      <h5 className="mt-0">Industries</h5>
+                      <IndustriesMenu />
+                    </Col>
+                  </Row>
+                </Container>
+              </PopoverBody>
+            </Popover>
+            <Modal
+              isOpen={isOpen}
+              toggle={toggle}
+              className="bot-side-menu"
+              backdrop="static"
+            >
+              <ModalBody>
+                <MenuSection toggle={toggle} />
+              </ModalBody>
+            </Modal>
+          </nav>
+        )}
+    </React.Fragment>
+
   );
 };
 
