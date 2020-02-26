599bbcf27739fdf3464d1061bf416abfa189487c
DaviD <albaktiar10@gmail.com>
2020-02-26 17:56:44 +0530

    updated home page

--- src/components/Landing/Home.js
+++ src/components/Landing/Home.js
@@ -4,6 +4,8 @@ import "../../styles/home.scss";
 import PreviewCompatibleImage from "../PreviewCompatibleImage";
 import arrow from "../../img/arrow.svg";
 import { Link } from "gatsby";
+import HeroImage from "../../img/hero_image.png";
+
 class Home extends React.Component {
   state = {};
   render() {
@@ -16,26 +18,35 @@ class Home extends React.Component {
               {/* <Col lg={1} md={2}></Col> */}
               <Col lg={7} md={6} sm={12} xs={12}>
                 <article className="gap-y">
-                  <h1 className="text-primary bold">{home.title}</h1>
-                  <h4 >{home.subTitle}</h4>
+                  {/* <h1 className="text-primary bold">{home.title}</h1> */}
+                  <h1 className="text-primary bold">
+                    Incede infuses IBM Watson AI into your business processes
+                    that you can trust
+                  </h1>
+                  <h4>
+                    Incede personalizes user experiences, streamlines business
+                    processes and workflow and sparks innovation by deploying
+                    IBM Watson AI solutions
+                  </h4>
+                  {/* <h4>{home.subTitle}</h4> */}
                   <div className="py-2">
-                    {/* <Link to="/solutions" className="btn btn-secondary btn-lg">
-                      {home.button}
-                      {"  "}
+                    <Link to="/solutions" className="btn btn-secondary btn-lg">
+                      Explore Our Solutions
                       <img src={arrow} alt="next" style={{ width: "12px" }} />
-                    </Link> */}
+                    </Link>
                   </div>
                   {/* <pre>{JSON.stringify(home, null, 2)}</pre> */}
                 </article>
               </Col>
               <Col lg={5} md={6} className="d-none d-md-block position-initial">
-                <div className="image-section">
-                  <PreviewCompatibleImage
+                <div className="image-section gap-y">
+                  {/* <PreviewCompatibleImage
                     imageInfo={{
                       image: home.image,
                       alt: `featured image thumbnail`
                     }}
-                  />
+                  /> */}
+                  <img src={HeroImage} alt="hero" />
                 </div>
               </Col>
             </Row>
