1f3ece9acaf11e1d95e07d7778c70c5631dd770a
Rathan <thoti.rathan@gmail.com>
2020-03-04 13:32:32 +0530

    removed case-studies from home page

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ class Main extends Component {
         {/* <AboutUs about={about} /> */}
         <MoreDetail />
         <WhatWeDo />
-        <CaseStudies />
+        {/* <CaseStudies /> */}
         {/* <ContactUs location={location} /> */}
       </React.Fragment>
     );
