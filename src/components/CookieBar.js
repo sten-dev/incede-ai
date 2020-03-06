76f650d0306e106b7afa4fe75e6c86a7f2daf829
Rathan <thoti.rathan@gmail.com>
2020-03-06 11:37:47 +0530

    added cookie bar

---
+++ src/components/CookieBar.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+class CookieBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="cookie-bar-container">
+          <div className="animate-bottom">
+            <Container fluid>
+              <Row className="d-flex align-items-center flex-column-reverse flex-sm-row">
+                <Col xs={11} className="p-0 p-sm-3 pb-2">
+                  <div className="body">
+                    <p>
+                      Welcome to <b>incede.ai</b> ! In order to provide a more
+                      relevant experience for you, we use cookies to enable some
+                      website functionality. Cookies help us see which articles
+                      most interest you; allow you to easily share articles on
+                      social media; permit us to deliver content, jobs and ads
+                      tailored to your interests and locations; and provide many
+                      other site benefits. For more information, please review
+                      our Cookies Policy and Privacy Statement.
+                    </p>
+                  </div>
+                </Col>
+                <Col className="col pt-2 p-sm-0 text-right text-sm-center">
+                  <img
+                    src="/img/close-icon.png"
+                    onClick={() => this.props.addCookie()}
+                    className="close-icon pointer"
+                  />
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default CookieBar;
--- src/components/LayoutBody.js
+++ src/components/LayoutBody.js
@@ -1,105 +1,130 @@
-import React, { Component } from 'react';
-import INavbar from './Navbar';
-import ChatMain from './Landing/bot/ChatMain';
-import Footer from './Footer';
-import { animateScroll as scroll } from 'react-scroll'
+import React, { Component } from "react";
+import INavbar from "./Navbar";
+import ChatMain from "./Landing/bot/ChatMain";
+import Footer from "./Footer";
+import { animateScroll as scroll } from "react-scroll";
+import CookieBar from "./CookieBar";
 class LayoutBody extends Component {
-    window
-    constructor(props) {
-        super(props);
-        this.state = {
-            isScrollHidden: false
-        }
-    }
-    componentDidMount() {
-        this.window = window
-        this.handleSession();
-        document.addEventListener('scroll', this.trackScrolling);
-        this.trackScrolling()
-    }
-
-    parseJwt = () => {
-        let token = this.getToken();
-        if (token) {
-            var base64Url = token.split('.')[1];
-            var base64 = base64Url.replace('-', '+').replace('_', '/');
-            return JSON.parse(this.window.atob(base64));
-        }
-        return '';
+  window;
+  constructor(props) {
+    super(props);
+    this.state = {
+      isScrollHidden: false,
+      isCookieSaved: false
     };
+  }
+  componentDidMount = async () => {
+    this.window = window;
+    this.handleSession();
+    document.addEventListener("scroll", this.trackScrolling);
+    this.trackScrolling();
+    await this.findCookie();
+  };
 
-    getToken = () => {
-        if (this.window && this.window.localStorage) {
-            let obj = this.window.localStorage.getItem('incedeAuthToken');
-            return obj;
-        }
-        return null;
-    };
-
-    isTokenExpired = () => {
-        var token = this.getToken();
-        if (token) {
-            let user = this.parseJwt();
-            var cur_time = new Date().getTime() / 1000;
-            if (user && user.exp && cur_time < user.exp) {
-                return false;
-            }
-            return true;
-        } else {
-            return true;
-        }
+  parseJwt = () => {
+    let token = this.getToken();
+    if (token) {
+      var base64Url = token.split(".")[1];
+      var base64 = base64Url.replace("-", "+").replace("_", "/");
+      return JSON.parse(this.window.atob(base64));
     }
+    return "";
+  };
 
-    handleSession = () => {
-        if (!window.location.pathname.startsWith("/login") && this.isTokenExpired()) {
-            window.location.href = "/login"
-        }
+  getToken = () => {
+    if (this.window && this.window.localStorage) {
+      let obj = this.window.localStorage.getItem("incedeAuthToken");
+      return obj;
     }
+    return null;
+  };
 
-    componentWillUnmount() {
-        document.removeEventListener('scroll', this.trackScrolling);
+  isTokenExpired = () => {
+    var token = this.getToken();
+    if (token) {
+      let user = this.parseJwt();
+      var cur_time = new Date().getTime() / 1000;
+      if (user && user.exp && cur_time < user.exp) {
+        return false;
+      }
+      return true;
+    } else {
+      return true;
     }
-    isBottom = (el) => {
-        if (el && el.getBoundingClientRect) {
-            return (el.getBoundingClientRect().bottom - window.innerHeight) <= 50;
-        }
-    }
-    trackScrolling = () => {
-        const wrappedElement = document.getElementById("layout_body")
-        if (this.isBottom(wrappedElement)) {
-            this.setState({
-                isScrollHidden: true
-            });
-        } else {
-            this.setState({
-                isScrollHidden: false
-            })
-        }
-    };
+  };
 
-    scrollMore = () => {
-        scroll.scrollMore(250);
-    };
-
-    render() {
-        return (
-            <section id="layout_body">
-                <INavbar page={this.props.page} />
-                <ChatMain page={this.props.page} />
-                <div>{this.props.children}</div>
-                {this.props.page !== "dashboard" && (
-                    <Footer />
-                )}
-                {this.state.isScrollHidden !== true && (
-                    <section className="scroll-to-bottom-container" onClick={this.scrollMore} >
-                        <svg viewBox="0 0 24 24">
-                            <path fill="#b3b3b3" d="M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M6,10L12,16L18,10L16.6,8.6L12,13.2L7.4,8.6L6,10Z" />
-                        </svg>
-                    </section>
-                )}
-            </section>
-        );
+  handleSession = () => {
+    if (
+      !window.location.pathname.startsWith("/login") &&
+      this.isTokenExpired()
+    ) {
+      window.location.href = "/login";
     }
+  };
+
+  findCookie = () => {
+    if (this.window && this.window.localStorage) {
+      let obj = this.window.localStorage.getItem("incedeCookie");
+      if (obj) {
+        this.setState({ isCookieSaved: true });
+      }
+    }
+  };
+  handleAddCookie = () => {
+    this.window.localStorage.setItem("incedeCookie", "1");
+    this.setState({ isCookieSaved: true });
+  };
+  componentWillUnmount() {
+    document.removeEventListener("scroll", this.trackScrolling);
+  }
+  isBottom = el => {
+    if (el && el.getBoundingClientRect) {
+      return el.getBoundingClientRect().bottom - window.innerHeight <= 50;
+    }
+  };
+  trackScrolling = () => {
+    const wrappedElement = document.getElementById("layout_body");
+    if (this.isBottom(wrappedElement)) {
+      this.setState({
+        isScrollHidden: true
+      });
+    } else {
+      this.setState({
+        isScrollHidden: false
+      });
+    }
+  };
+
+  scrollMore = () => {
+    scroll.scrollMore(250);
+  };
+
+  render() {
+    return (
+      <section id="layout_body">
+        <INavbar page={this.props.page} />
+        <ChatMain page={this.props.page} />
+        <div>{this.props.children}</div>
+        {this.props.page !== "dashboard" && <Footer />}
+        {this.state.isCookieSaved === false && (
+          <CookieBar addCookie={() => this.handleAddCookie()} />
+        )}
+        {this.state.isScrollHidden !== true && (
+          <section
+            className="scroll-to-bottom-container"
+            onClick={this.scrollMore}
+          >
+            <svg viewBox="0 0 24 24">
+              <path
+                fill="#b3b3b3"
+                d="M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M6,10L12,16L18,10L16.6,8.6L12,13.2L7.4,8.6L6,10Z"
+              />
+            </svg>
+          </section>
+        )}
+      </section>
+    );
+  }
 }
 
 export default LayoutBody;
