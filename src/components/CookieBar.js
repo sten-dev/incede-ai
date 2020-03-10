0e1a09d6e1bcb55cd32d5c565038c7a02d6dc17a
paeruva+1-at-154091047068 <paeruva+1-at-154091047068>
2020-03-10 21:25:35 +0530

    changed cookie bar content

--- src/components/CookieBar.js
+++ src/components/CookieBar.js
@@ -16,15 +16,9 @@ class CookieBar extends Component {
               <Row className="d-flex align-items-center flex-column-reverse flex-sm-row">
                 <Col xs={11} className="p-0 p-sm-3 pb-2">
                   <div className="body">
+
                     <p>
-                      Welcome to <b>locussolutions.com</b> ! In order to provide
-                      a more relevant experience for you, we use cookies to
-                      enable some website functionality. Cookies help us see
-                      which articles most interest you; allow you to easily
-                      share articles on social media; permit us to deliver
-                      content, jobs and ads tailored to your interests and
-                      locations; and provide many other site benefits. For more
-                      information, please review our{" "}
+                      Welcome to <strong>locussolutions.com!</strong> In order to provide a more relevant experience for you, we use cookies to enable some website functionality. Cookies help us see which articles most interest you; allow you to easily share articles on social media; permit us to deliver content, jobs and ads tailored to your interests and locations; and provide many other site benefits. For more information, please review our{" "}
                       <Link to="/about/cookie-policy">Cookies Policy</Link> and{" "}
                       <Link to="/about/privacy-policy">Privacy Policy</Link>.
                     </p>
