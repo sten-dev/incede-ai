ac1764f367689e901abf631be83b85989f834ebd
Rathan <thoti.rathan@gmail.com>
2020-03-11 18:12:11 +0530

    fixed cookie bar issue to show colse icon

--- src/components/CookieBar.js
+++ src/components/CookieBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "gatsby";
+import closeImage from "../img/close-black.svg";
 
 class CookieBar extends Component {
   constructor(props) {
@@ -16,9 +17,10 @@ class CookieBar extends Component {
               <Row className="d-flex align-items-center flex-column-reverse flex-sm-row">
                 <Col xs={11} className="p-0 p-sm-3 pb-2">
                   <div className="body">
-
                     <p>
-                      Welcome to Incede.ai! In order to provide a more relevant experience for you, we utilize cookies. For more information, please review our{" "}
+                      Welcome to Incede.ai! In order to provide a more relevant
+                      experience for you, we utilize cookies. For more
+                      information, please review our{" "}
                       <Link to="/about/cookie-policy">Cookies Policy</Link>
                       {/* {" "}and{" "}
                       <Link to="/about/privacy-policy">Privacy Policy</Link>. */}
@@ -27,7 +29,7 @@ class CookieBar extends Component {
                 </Col>
                 <Col className="col pt-2 p-sm-0 text-right text-sm-center">
                   <img
-                    src="/img/close-icon.png"
+                    src={closeImage}
                     onClick={() => this.props.addCookie()}
                     className="close-icon pointer"
                   />
