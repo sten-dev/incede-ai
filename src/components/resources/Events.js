7adf07893b0d17cba38f674c0833647ce6e82411
Kutub <kutubuddin2528@gmail.com>
2021-01-05 15:42:52 +0530

    header added for resources & 2 basic compont added

---
+++ src/components/resources/Events.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "reactstrap/lib/Container";
+
+const Events = () => {
+  return (
+    <section className="industry-solution-content gap-y-half" id="1">
+      <Container>
+        <h1 className="text-left heading mb-5">Events</h1>
+      </Container>
+    </section>
+  );
+};
+
+export default Events;
---
+++ src/components/resources/ResourceLibrary.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "reactstrap/lib/Container";
+
+const ResourceLibrary = () => {
+  return (
+    <section className="industry-solution-content gap-y-half" id="1">
+      <Container>
+        <h1 className="text-left heading mb-5">Resource Library</h1>
+      </Container>
+    </section>
+  );
+};
+
+export default ResourceLibrary;
