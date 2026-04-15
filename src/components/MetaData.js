5445572935d1ebed7b193f48868d4351ff1360f5
Phaneendra <paeruva@contenterra.com>
2026-04-15 16:21:22 +0530

    fixed issue

--- src/components/MetaData.js
+++ src/components/MetaData.js
@@ -320,6 +320,7 @@ const MetaData = ({ pageTitle, pageDescription, keyWords }) => {
             transportOptions: {
               polling: true
             },
+            streaming: false,
             rootElementID: "root",
             deploymentPlatform: "ibmcloud",
             crn: "crn:v1:bluemix:public:watsonx-orchestrate:us-south:a/0781f29958be4f588e177e1250f85e99:01472fb1-37b5-46dd-8479-0e23044b68d2::",
